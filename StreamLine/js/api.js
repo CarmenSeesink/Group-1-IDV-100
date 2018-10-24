cd90f538747ae3ef78f4d6034c26b98f48cd588e
Unknown <unknown@example.com>
2018-10-24 23:23:10 +0200

    Lerato Individual Page
    Attempted Carlo's Api method
    Did some bootstraps.
    Edited CSS

--- StreamLine/js/api.js
+++ StreamLine/js/api.js
@@ -2,18 +2,26 @@ $(function () {
     var request = new XMLHttpRequest();
 
     // Open a new connection, using the GET request on the URL endpoint
-    request.open('GET', 'https://ghibliapi.herokuapp.com/films', true);
+    request.open('GET', 'https://omdbapi.com/?i=tt3896198&apikey=5d0c0e4f', true);
     
-    request.onload = function () {
-      // Begin accessing JSON data here
-    var data = JSON.parse(this.response);
-
-    data.forEach(movie => {
-    // Log each movie's title
-    console.log(movie.title);
-    });
-    }
-    
-    // Send request
     request.send();
+    
+    request.onload = function(){
+        var data = JSON.parse(this.response);
+        
+        var title = data.Title;
+        var poster = data.Poster;
+        var genre = data.Genre
+        
+            console.log(title);
+            console.log(poster);
+            console.log(genre);
+        
+        
+        
+        function create(){
+            $(".poster-container").append("<div class='column-1'><div class='image-1'" + poster + "></div><p>" + title + "</p></div>");
+            
+        }
+    }
 });
