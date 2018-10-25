83c3fdfa62f8f02bca633f340683a51cf737bb78
Unknown <180145@virtualwindow.co.za>
2018-10-25 10:30:03 +0200

    Api libray page
    Api fixed

--- StreamLine/js/api.js
+++ StreamLine/js/api.js
@@ -1,7 +1,6 @@
 //Requested movies array
 var moviesArray = ["venom", "goosebumps+2", "first+man", "halloween", "johnny+English+Strikes+Again"];
 
-<<<<<<< HEAD
 //Requested content from the array
 var movieTitle = '';
 var movieImage = '';
@@ -102,29 +101,3 @@ for (var i = 0; i < moviesArray.length; i++) {
   //     }
   // });
   
-=======
-    // Open a new connection, using the GET request on the URL endpoint
-    request.open('GET', 'https://omdbapi.com/?i=tt3896198&apikey=5d0c0e4f', true);
-    
-    request.send();
-    
-    request.onload = function(){
-        var data = JSON.parse(this.response);
-        
-        var title = data.Title;
-        var poster = data.Poster;
-        var genre = data.Genre
-        
-            console.log(title);
-            console.log(poster);
-            console.log(genre);
-        
-        
-        
-        function create(){
-            $(".poster-container").append("<div class='column-1'><div class='image-1'" + poster + "></div><p>" + title + "</p></div>");
-            
-        }
-    }
-});
->>>>>>> master
