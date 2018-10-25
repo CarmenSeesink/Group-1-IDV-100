885cc69f54d5c682ae076a8d0fc2fd0f1d46141d
Unknown <180145@virtualwindow.co.za>
2018-10-25 10:56:53 +0200

    Api add to index
    Added api to index
    Fixed some files with library page

--- StreamLine/js/index.js
+++ StreamLine/js/index.js
@@ -29,17 +29,13 @@ function getData(item, loop){
     
     movieImage = response.Poster;
     movieTitle = response.Title;
-    movieGenre = response.Genre;
-    imdbRating = response.imdbRating;
-    movieYear = response.Year;
-    movieRating = response.Rated;
-    moviePlot = response.Plot;
+    
     id = response.imdbID;
 
     
 
     $(function() { 
-      $(".row1").append("<div id=" + "con" + id + "><div class='card--content'><img class='poster' src=" + movieImage + "></div></div>");
+      $(".row1").append("<div class='card--content'><div class='poster'><div class='poster-overlay'><img id='movies-z' src=" + movieImage + " style='width:100%;' style='height:100%;'></div></div></div>");
     });
 
   });
@@ -55,62 +51,6 @@ for (var i = 0; i < moviesArray.length; i++) {
 }
 
 });
-
-
-
-//Coming Soon Api function
-
-$(function(){
 
 //Requested movies array
-var moviesArray = ["The+Nutcracker+and+the+Four+Realms", "The+Grinch&y=2018", "Nobody's+Fool", "Ralph+Breaks+the+Internet", "robin+hood&y=2018", "Mortal+Engines"];
-
-//Requested content from the array
-var movieTitle = '';
-var movieImage = '';
-var movieGenre = '';
-var imdbRating = '';
-var movieYear = '';
-var movieRating = '';
-var moviePlot = '';
-var movieLanguage = '';
-var id = '';
-
-
-function getData(item, loop){
-
-  $.ajax({
-    url: 'https://www.omdbapi.com/?t=' + encodeURI(item, loop) + '&apikey=5d0c0e4f',
-    dataType: "json",
-    async: true
-  }).done(function(response) {
-
-    console.log(response);
-    
-    movieImage = response.Poster;
-    movieTitle = response.Title;
-    movieGenre = response.Genre;
-    imdbRating = response.imdbRating;
-    movieYear = response.Year;
-    movieRating = response.Rated;
-    moviePlot = response.Plot;
-    id = response.imdbID;
-
-    
-
-    $(function() { 
-      $(".row2").append("<div id=" + "con" + id + "><div class='card--content'><img class='poster' src=" + movieImage + "></div></div>");
-    });
-
-  });
-  
-
-}
-
-for (var i = 0; i < moviesArray.length; i++) {  
-  
-  console.log(moviesArray[i]);
-  getData(moviesArray[i], i)
-  
-}
-});
+// var moviesArray = ["The+Nutcracker+and+the+Four+Realms", "The+Grinch&y=2018", "Nobody's+Fool", "Ralph+Breaks+the+Internet", "robin+hood&y=2018", "Mortal+Engines"];
